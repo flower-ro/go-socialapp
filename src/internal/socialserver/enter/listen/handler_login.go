cd197330d718cb024029514a7be80b75bb001b99
Wisdom <183021882@qq.com>
2023-11-08 09:38:17 +0800

    support multi device

--- src/internal/socialserver/enter/listen/handler_login.go
+++ src/internal/socialserver/enter/listen/handler_login.go
@@ -1,13 +1,11 @@
 package listen
 
 import (
-	"context"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/marmotedu/errors"
 	"github.com/marmotedu/iam/pkg/log"
-	"github.com/otiai10/copy"
+	//"github.com/otiai10/copy"
 	"go-socialapp/internal/pkg/third-party/whatsapp"
-	utils "go-socialapp/internal/pkg/util"
+	//utils "go-socialapp/internal/pkg/util"
 	"go-socialapp/internal/socialserver/cache/loggedin"
 	whatsappApi "go-socialapp/internal/socialserver/client/whatsapp"
 	ws2 "go-socialapp/internal/socialserver/ws"
@@ -22,6 +20,7 @@ func (w *WaListen) handlerLoginMessage(message whatsapp.BroadcastMessage) error 
 		if err != nil {
 			message.WaClient.WaCli.Disconnect()
 			log.Errorf("%+v", err)
+			ws2.Manager.BroadcastMsg(ws2.Message{Code: MessageTypeLoginFail})
 		}
 	}()
 	if strings.Contains(message.Result.(string), ":") {
@@ -37,33 +36,33 @@ func (w *WaListen) handlerLoginMessage(message whatsapp.BroadcastMessage) error 
 		return errors.Wrap(err, " ")
 	}
 	newPath := filepath.Join(whatsapp.PathSessions, phone+".db")
-	utils.RemoveFile(0, newPath)
-	err = copy.Copy(message.WaClient.Path, newPath)
-	if err != nil {
-		log.Errorf("Phone %s,copy sessionTmp %s  to session file err %s", phone, message.WaClient.Path, err.Error())
-		ws2.Manager.BroadcastMsg(ws2.Message{Code: MessageTypeLoginFail})
-		return err
-	}
+	//utils.RemoveFile(0, newPath)
+	//err = copy.Copy(message.WaClient.Path, newPath)
+	//if err != nil {
+	//	log.Errorf("Phone %s,copy sessionTmp %s  to session file err %s", phone, message.WaClient.Path, err.Error())
+	//	ws2.Manager.BroadcastMsg(ws2.Message{Code: MessageTypeLoginFail})
+	//	return err
+	//}
 
 	newDb, err := whatsapp.NewWaDB(newPath)
 	if err != nil {
-		log.Errorf("Phone %s,NewWaDB %s err %s", phone, message.Result.(string), err.Error())
-		ws2.Manager.BroadcastMsg(ws2.Message{Code: MessageTypeLoginFail})
-		return err
+		return errors.Wrapf(err, "Phone %s,NewWaDB for device %s", phone, message.Result.(string))
 	}
-
+	d, err := message.WaClient.Db.GetFirstDevice()
+	if err != nil {
+		return errors.Wrapf(err, "Phone %s, db GetFirstDevice for %s", phone, message.Result.(string))
+	}
+	err = newDb.PutDevice(d)
+	if err != nil {
+		return errors.Wrapf(err, "Phone %s, put device for %s", phone, message.Result.(string))
+	}
 	err = w.srv.Accounts().CreateOrUpdate(phone, message.Result.(string))
 	if err != nil {
-		log.Errorf("Phone %s,NewWaClientWithDevice err %s", phone, err.Error())
-		ws2.Manager.BroadcastMsg(ws2.Message{Code: MessageTypeLoginFail})
-		return err
+		return errors.Wrapf(err, "Phone %s,NewWaClientWithDevice", phone)
 	}
 	factory := whatsappApi.NewFactory(message.WaClient.WaCli, newDb)
 	loggedin.WaApiCache.Put(phone, factory)
 	ws2.Manager.BroadcastMsg(ws2.Message{Code: whatsapp.MessageTypeLogin, Result: message.Result})
-
-	spew.Dump("------------------vb--------")
-	spew.Dump(factory.User().MyListGroups(context.Background()))
 	return nil
 
 }
