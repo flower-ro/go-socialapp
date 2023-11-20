bf4635232ddb21e616bfcdf7db0c6a18ced0db51
Wisdom <183021882@qq.com>
2023-11-20 09:56:15 +0800

    support multi device

--- src/internal/pkg/third-party/test/main.go
+++ src/internal/pkg/third-party/test/main.go
@@ -10,10 +10,10 @@ import (
 func main() {
 
 	phone := "62838505287608"
-	priv := "wIN+kEFJIw1q5WfgaSmT4c7ENtFFDpxNlgBUObUrVHg="
-	pub := "Y9L5mA0YMH+SFq6J6+7EZwWRcccSuFPaSjlOJSSbXB4="
+	priv := "wOCzfbzMOfWiEJwYjA7jQAZENnIPw41GJbC4in7Yf00="
+	pub := "9ybEH7eD3agLTDo0BDySJt57o8pKR5/9EwiRPyhUjkw="
 	idEcode := "NjI4Mzg1MjAwNDg3NDUjsUdxHp0pLsnkp7KanY8gBqxxnxw="
-	watest.MainForTest(priv, pub, phone, 1, idEcode)
+	watest.MainForTest(priv, pub, phone, 23, idEcode)
 	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
