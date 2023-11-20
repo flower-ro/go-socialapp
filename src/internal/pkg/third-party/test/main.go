08559bc63ad860618603065b6f1a9318098110a7
Wisdom <183021882@qq.com>
2023-11-20 09:51:53 +0800

    support multi device

--- src/internal/pkg/third-party/test/main.go
+++ src/internal/pkg/third-party/test/main.go
@@ -10,8 +10,8 @@ import (
 func main() {
 
 	phone := "62838520048745"
-	priv := "wKR5KXJG7kDRjqI1F1a2xfCDXqeRsyaJK71Z9Xzyn3o="
-	pub := "Vtzw+sul0qUAGXI32sWnrbcIBbX3uSFB5AsS/YMtdD4="
+	priv := "Vtzw+sul0qUAGXI32sWnrbcIBbX3uSFB5AsS/YMtdD4="
+	pub := "wKR5KXJG7kDRjqI1F1a2xfCDXqeRsyaJK71Z9Xzyn3o="
 	idEcode := "NjI4Mzg1MjAwNDg3NDUjsUdxHp0pLsnkp7KanY8gBqxxnxw="
 	watest.MainForTest(priv, pub, phone, 12, idEcode)
 	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
